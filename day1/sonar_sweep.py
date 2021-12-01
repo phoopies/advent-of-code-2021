658e1f3e83ec6c77a98b9ffcfb4cba522b3de0c5
stefu <sotajagic@gmail.com>
2021-12-01 19:45:35 +0200

    day 1 sliding windows

--- day1/sonar_sweep.py
+++ day1/sonar_sweep.py
@@ -1,14 +1,31 @@
-file_name: str = "day1/sonar_sweep_input"
-count = 0
+from typing import List
 
-def increased(a: int, b: int):
-    return a < b
+
+file_name: str = "day1/sonar_sweep_input"
+window_size: int = 3
+count: int = 0
+
+def increased(a: List[int], b: List[int]) -> bool:
+    return sum(a) < sum(b)
+
+def sum(arr: List[int]) -> int:
+    s = 0
+    for a in arr: s += a
+    return s
+
+def next_window(arr: List[int], a: int) -> List[int]:
+    new_arr = arr.copy()
+    new_arr.pop(0)
+    new_arr.append(a)
+    return new_arr
 
 with open(file_name, 'r') as f:
-    p = f.readline()
-    prev = int(p)
+    prev = []
+    for i in range(window_size):
+        l = f.readline()
+        prev.append(int(l))
     while c := f.readline():
-        current = int(c)
+        current = next_window(prev, int(c))
         if increased(prev, current): count += 1
         prev = current
 
