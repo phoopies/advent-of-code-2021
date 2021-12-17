2e36fda46b9bd697d41f909bf1cf9aa6c2e366bf
stefu <sotajagic@gmail.com>
2021-12-17 23:25:11 +0200

    reformat day 17

--- day17/trick_shot.py
+++ day17/trick_shot.py
@@ -12,14 +12,15 @@ as from y_top to y (== 0)
 -> return sum([i for i in range(1, -1*(lowest_y)])
 '''
 
+
 class Trench:
     def __init__(self, x1: int, y1: int, x2: int, y2: int) -> None:
         self.x = (x1, x2)
         self.y = (y1, y2)
-    
+
     def __contains__(self, point: Tuple[int, int]) -> bool:
         return self.inside(point[0], point[1])
-    
+
     def inside(self, x: int, y: int) -> bool:
         return self.inside_x(x) and self.inside_y(y)
 
@@ -28,6 +29,8 @@ class Trench:
 
     def inside_y(self, y: int) -> bool:
         return self.y[0] <= y <= self.y[1]
+
+
 class Probe:
     drag: int = 1
     gravity: int = -1
@@ -37,38 +40,40 @@ class Probe:
         self.y = 0
         self.x_vel = 0
         self.y_vel = 0
- 
+
     def set_velocity_x(self, vel: int) -> None:
         self.x_vel = vel
-    
+
     def set_velocity_y(self, vel: int) -> None:
         self.y_vel = vel
-    
+
     def set_velocity(self, vel_x: int, vel_y: int) -> None:
         self.set_velocity_x(vel_x)
         self.set_velocity_y(vel_y)
-    
+
     def handle_gravity(self) -> None:
         self.set_velocity_y(self.y_vel + Probe.gravity)
-    
+
     def handle_drag(self) -> None:
         direction = 0
-        if self.x_vel > 0: direction = -Probe.drag
-        elif self.x_vel < 0: direction = Probe.drag
+        if self.x_vel > 0:
+            direction = -Probe.drag
+        elif self.x_vel < 0:
+            direction = Probe.drag
         self.set_velocity_x(self.x_vel + direction)
-    
+
     def step(self) -> Tuple[int, int]:
         self.x += self.x_vel
         self.y += self.y_vel
         self.handle_gravity()
         self.handle_drag()
         return self.x, self.y
-    
+
     def shoot(self, x_vel: int, y_vel: int, steps: int = 100) -> List[Tuple[int, int]]:
         self.set_velocity(x_vel, y_vel)
         projectory = [self.step() for _ in range(steps)]
         return projectory
-    
+
     def shoot_until(self, x_vel: int, y_vel: int, target: Trench) -> bool:
         self.set_velocity(x_vel, y_vel)
         y_low = min(target.y)
@@ -78,31 +83,23 @@ class Probe:
             if self.y < y_low or self.x > x_max:
                 return False
         return True
-    
+
     def reset(self) -> None:
         self.x = 0
         self.y = 0
 
+
 class Launcher:
     def __init__(self, target: Trench) -> None:
         self.target = target
-    
+
     def high_shot(self) -> int:
-        '''
-        Conservation of energy / momentum -> there exists some y != start_y so that y == start_y (== 0)
-        Now we wan't the last step to be as big as possible -> y = - y_low, assuming y_low <= 0
-        now realize that with max height shot we take the same amount of steps from y_start to y_top
-        as from y_top to y (== 0)
-        '''
         return sum([i for i in range(1, -1*(min(self.target.y)))])
-    
+
     def low_shot(self) -> int:
         return min(self.target.y)
-        
+
     def fast_shot(self) -> int:
-        '''
-        Fastest shot we can do so that still hits the target
-        '''
         return max(self.target.x)
 
     def slow_shot(self) -> int:
@@ -110,13 +107,13 @@ class Launcher:
         while not self.target.inside_x(sum([i for i in range(1, x_vel+1)])):
             x_vel += 1
         return x_vel
-    
+
     def possible_shots(self) -> int:
         min_x = self.slow_shot()
         max_x = self.fast_shot()
         min_y = self.low_shot()
         max_y = self.high_shot()
-        
+
         shots = 0
         probe = Probe()
         for x in range(min_x, max_x+1):
@@ -125,15 +122,16 @@ class Launcher:
                     shots += 1
                 probe.reset()
         return shots
-    
+
     def shoot(self, x_vel: int, y_vel: int, steps: int) -> bool:
         probe = Probe()
         projectory = probe.shoot(x_vel, y_vel, steps)
         print(projectory)
         return any([point in self.target for point in projectory])
 
+
 def solve(filename: str = "input") -> Tuple[int, int]:
-    x1, x2, y1, y2 = 0, 0, 0, 0 
+    x1, x2, y1, y2 = 0, 0, 0, 0
     with open(f"day17/{filename}", 'r') as f:
         line = f.readline().strip()
         line = line.replace("target area: ", "")
@@ -143,6 +141,7 @@ def solve(filename: str = "input") -> Tuple[int, int]:
     target = Trench(x1, y1, x2, y2)
     launcher = Launcher(target)
     return launcher.high_shot(), launcher.possible_shots()
+
 
 if __name__ == "__main__":
     # test_target = Trench(20, -10, 30, -5)
